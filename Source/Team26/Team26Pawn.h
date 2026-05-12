e8c0a5333d372ddffe4beb0f0b12cdf06055e57f
minseo <min058728@gmail.com>
2026-05-12 11:03:17 +0900

    [Fix] [KMS] . 자율주행 on/off 및 브레이크

--- Source/Team26/Team26Pawn.h
+++ Source/Team26/Team26Pawn.h
@@ -1,76 +1,57 @@
 ﻿// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "WheeledVehiclePawn.h"
 #include "Team26Pawn.generated.h"
-
 class UCameraComponent;
 class USpringArmComponent;
 class UInputAction;
 class UChaosWheeledVehicleMovementComponent;
 struct FInputActionValue;
-
 DECLARE_LOG_CATEGORY_EXTERN(LogTemplateVehicle, Log, All);
-
 UCLASS(abstract)
 class ATeam26Pawn : public AWheeledVehiclePawn
 {
 	GENERATED_BODY()
-
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* FrontSpringArm;
-
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FrontCamera;
-
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* BackSpringArm;
-
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* BackCamera;
-
 	TObjectPtr<UChaosWheeledVehicleMovementComponent> ChaosVehicleMovement;
-
 protected:
-
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
 	UInputAction* SteeringAction;
-
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
 	UInputAction* ThrottleAction;
-
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
 	UInputAction* BrakeAction;
-
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
 	UInputAction* HandbrakeAction;
-
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
 	UInputAction* LookAroundAction;
-
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
 	UInputAction* ToggleCameraAction;
-
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
 	UInputAction* ResetVehicleAction;
-
 	bool bFrontCameraActive = false;
-
 public:
 	ATeam26Pawn();
-
 	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
 	virtual void Tick(float Delta) override;
-
 	// AI 자율주행 제어 함수
 	void DoThrottle(float Value);  // 액셀
 	void DoBrake(float Value);     // 브레이크
 	void DoSteering(float Value);  // 핸들
 
-protected:
+	// [추가] 자율주행 on/off (true = 자율주행, false = 수동)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AutoDrive")
+	bool bAutoDrive = true;
 
+protected:
 	void Steering(const FInputActionValue& Value);
 	void Throttle(const FInputActionValue& Value);
 	void Brake(const FInputActionValue& Value);
@@ -81,10 +62,8 @@ protected:
 	void LookAround(const FInputActionValue& Value);
 	void ToggleCamera(const FInputActionValue& Value);
 	void ResetVehicle(const FInputActionValue& Value);
-
 	UFUNCTION(BlueprintImplementableEvent, Category = "Vehicle")
 	void BrakeLights(bool bBraking);
-
 public:
 	FORCEINLINE USpringArmComponent* GetFrontSpringArm() const { return FrontSpringArm; }
 	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FrontCamera; }
