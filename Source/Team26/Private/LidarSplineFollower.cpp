7a1b923cb1c6e66bf6be70a1bfcacf0faf74cd7b
minseo <min058728@gmail.com>
2026-05-22 10:22:39 +0900

    [add][강민서]. 자율주행시 방향 전환

---
+++ Source/Team26/Private/LidarSplineFollower.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#include "LidarSplineFollower.h"
+#include "Team26/Team26Pawn.h"
+#include "Sensor/LidarSensorComponent.h"
+
+float ULidarSplineFollower::ComputeExtraSteer() const
+{
+	if (!OwnerPawn.IsValid()) return 0.f;
+
+	ULidarSensorComponent* Lidar = OwnerPawn->FindComponentByClass<ULidarSensorComponent>();
+	if (!Lidar) return 0.f;
+
+	const float DistFront = Lidar->GetClosestForwardDistance();
+	const float DistLeft = Lidar->GetClosestForwardLeft();  
+	const float DistRight = Lidar->GetClosestForwardRight(); 
+
+	// 장애물이 충분히 멀면 회피 안함
+	if (DistFront > AvoidDistance) return 0.f;
+
+	// 왼쪽이 더 가까우면 오른쪽으로, 오른쪽이 더 가까우면 왼쪽으로
+	if (DistLeft < DistRight)
+		return AvoidSteerStrength;   // 오른쪽
+	else
+		return -AvoidSteerStrength;  // 왼쪽
+}
+
---
+++ Source/Team26/Public/LidarSplineFollower.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "CoreMinimal.h"
+#include "Component/SplineFollowerComponent.h"
+#include "Sensor/LidarSensorComponent.h"
+#include "LidarSplineFollower.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class TEAM26_API ULidarSplineFollower : public USplineFollowerComponent
+{
+	GENERATED_BODY()
+
+protected:
+	// [추가] [강민서] 장애물 회피 조향
+	virtual float ComputeExtraSteer() const override;
+
+private:
+	// [추가] [강민서] 회피 시작 거리 (기본 1000cm = 10m)
+	UPROPERTY(EditAnywhere, Category = "LidarAvoid")
+	float AvoidDistance = 1000.f;
+
+	// [추가] [강민서] 회피 조향 강도 (기본 0.5)
+	UPROPERTY(EditAnywhere, Category = "LidarAvoid")
+	float AvoidSteerStrength = 0.5f;
+};
